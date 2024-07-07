c3fc90adce4838a3d3c3e66762cca6a8f2b6fe6c
angurjar <anjali755577@gmail.com>
2024-07-07 11:44:35 +0530

    updated

--- m01/src/components/dashboard.jsx
+++ m01/src/components/dashboard.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../styles/Dashboard.css'; // Import Dashboard CSS
+
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Dashboard</h2>
-     
+    <div className="dashboard-container">
+      <h2 className="dashboard-heading">Dashboard</h2>
+      <div className="dashboard-section">
+        <h3>Total Leaves</h3>
+        <ul>
+      
+        </ul>
+      </div>
+      <div className="dashboard-section">
+        <h3>Attendance</h3>
+        <ul>
+          {/* List of attendance items */}
+        </ul>
+      </div>
+      <div className="dashboard-section">
+        <h3>Employees</h3>
+        <ul>
+          {/* List of employees */}
+        </ul>
+      </div>
     </div>
   );
 };
